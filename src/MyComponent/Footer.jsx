0b99dbae3e85b87962c8b4a2ef7060cb06696566
nimeshlathiya <142136394+NimeshLathiya@users.noreply.github.com>
2024-02-25 16:53:16 +0530

    Update Footer.jsx

--- src/MyComponent/Footer.jsx
+++ src/MyComponent/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <h2 className="Footer container-fluid mb-0 lh-1 position-fixed bottom-0 g-0">
-      <p>Amar Infotech</p>
+      <p>Nimesh Lathiya</p>
     </h2>
   );
 };
